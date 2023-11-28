5678ad18ef818171ad49ffe4264cab6489017139
Joongwon Lee <wnddnjs823@ajou.ac.kr>
2023-11-29 01:42:44 +0900

    Update 이미지 검색 앱 Stream을 활용하여 데이터 다루기

--- image_search/lib/data/photo_provider.dart
+++ image_search/lib/data/photo_provider.dart
@@ -1,18 +1,29 @@
 
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:image_search/data/api.dart';
 import 'package:image_search/model/Photo.dart';
 
 class PhotoProvider extends InheritedWidget {
   final PixabayApi api;
+  List<Photo> _photos = [];
 
-  const PhotoProvider({required this.api, required super.child, super.key});
+  final _photoStreamController = StreamController<List<Photo>>()..add([]);
+  Stream<List<Photo>> get photoStream => _photoStreamController.stream;
+
+  PhotoProvider({required this.api, required super.child, super.key});
 
   // 다른 위젯에 자유롭게 얹을 수 있는?? 구글링
   static PhotoProvider of(BuildContext context) {
     final PhotoProvider? result = context.dependOnInheritedWidgetOfExactType<PhotoProvider>();
     assert(result != null, 'No PixabayApi found in context'); // assert가 null이면, 에러 발생
     return result!;
+  }
+
+  Future<void> fetch(String query) async {
+    final result = await api.fetch(query);
+    _photoStreamController.add(result);
   }
 
   @override
