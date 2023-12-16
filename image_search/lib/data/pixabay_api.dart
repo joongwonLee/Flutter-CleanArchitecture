287395d58fccdae03e247d1a51a2bbdebed31b7f
Joongwon Lee <wnddnjs823@ajou.ac.kr>
2023-12-16 23:52:49 +0900

    Update 이미지 검색 앱 Mockito를 활용한 테스트

--- image_search/lib/data/pixabay_api.dart
+++ image_search/lib/data/pixabay_api.dart
@@ -5,11 +5,14 @@ import 'package:image_search/data/photo_api_repository.dart';
 import '../model/Photo.dart';
 
 class PixabayApi implements PhotoApiRepository {
-  final baseUrl = 'https://pixabay.com/api/';
-  final key = '40944834-a8b088f37e0776293e80ed326';
+  static const baseUrl = 'https://pixabay.com/api/';
+  static const key = '40944834-a8b088f37e0776293e80ed326';
 
-  Future<List<Photo>> fetch(String query) async {
-    final response = await http.get(Uri.parse(''
+  Future<List<Photo>> fetch(String query, {http.Client? client}) async {
+
+    client ??= http.Client();
+
+    final response = await client.get(Uri.parse(''
         '$baseUrl?key=$key&q=$query&image_type=photo'));
 
     Map<String, dynamic> jsonResponse = jsonDecode(response.body);
